1c5e9f477c76818d198fbef73b4d2963b957324c
Leonardo Hernandez <leonardo.gomes@venicetech.com.br>
2023-06-26 18:30:32 -0300

    custom-command

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,15 +3,37 @@ describe('Home page', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('The h1 contains the correct text', () => {
-    cy.get('[data-test="hero-heading"]').contains(
-      'Testing Next.js Applications with Cypress'
-    )
+  context("Hero section", () => {
+    it('The h1 contains the correct text', () => {
+      //cy.get('[data-test="hero-heading"]').contains(
+      //  'Testing Next.js Applications with Cypress'
+      //)
+      cy.getByData("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
+
+    it('the features on the home page are correct', () => {
+      cy.get('dt').eq(0).contains('4 Courses')
+      cy.get('dt').eq(1).contains('25+ Lessons')
+      cy.get('dt').eq(2).contains('Free and Open Source')
+    })
   })
 
-  it.only('the features on the home page are correct', () => {
-    cy.get('dt').eq(0).contains('4 Courses')
-    cy.get('dt').eq(1).contains('25+ Lessons')
-    cy.get('dt').eq(2).contains('Free and Open Source')
+  context("Courses section", () => {
+    it.only("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+
+    it.only("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")      
+    })
+
+    it.only("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")      
+    })
   })
 })
---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,26 @@
+describe("Newsletter Sbuscribe Form", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000")
+    })
+
+    it("ALLOWS users to subscribe to the email list", () => {
+        cy.getByData("email-input").type("leonardo.gomes@venicetech.com.br")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains("leonardo.gomes@venicetech.com.br")
+    })
+
+    it("does NOT allows an invalid email address", () => {
+        cy.getByData("email-input").type("leonardo.gomes")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+    })
+
+    it("does NOT allow an already subscribed email", () => {
+        cy.getByData("email-input").type("john@example.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("server-error-message")
+            .should("exist")
+            .contains("already exists. Please use a different email address.")
+    })
+})
+
