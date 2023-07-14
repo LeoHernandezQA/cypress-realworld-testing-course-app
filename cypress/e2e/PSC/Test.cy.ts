8332513acdfab7e8f15902fd9a9a96e8519330bd
Leonardo Hernandez <leonardo.gomes@venicetech.com.br>
2023-07-14 15:42:55 -0300

    new-intercept-adjusts

--- cypress/e2e/PSC/Test.cy.ts
+++ cypress/e2e/PSC/Test.cy.ts
@@ -1,16 +1,22 @@
 describe('Ver tela de login', () => {
   beforeEach(() => {
     cy.visit('http://a1aaa408818b145dbb82eacaa2d35368-588005589.sa-east-1.elb.amazonaws.com')
-    
-    cy.get("body > div:nth-child(6) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > input:nth-child(2)").type('leonardo.gomes@venicetech.com.br')
-    cy.get("body > div:nth-child(6) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > input:nth-child(3)").type('teste')
-    cy.get("body > div:nth-child(6) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > div > button").should('be.enabled').click() 
 
-    cy.intercept("https://demhcnslkt3b9.cloudfront.net/api/price/preorder/search", (req) => {
+    cy.intercept("https://demhcnslkt3b9.cloudfront.net/api/price/preorder/search", { fixture: 'preOrder' }).as("OneRegisterResponse")
+
+    cy.fixture("usersPSC").then((users) => {
+      cy.get("body > div:nth-child(6) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > input:nth-child(2)").type(users.username)
+      cy.get("body > div:nth-child(6) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > input:nth-child(3)").type(users.password)
+      cy.get("body > div:nth-child(6) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > div > button").should('be.enabled').click()
+    })
+
+   cy.wait('@OneRegisterResponse')
+
+    /*cy.intercept("https://demhcnslkt3b9.cloudfront.net/api/price/preorder/search", (req) => {
       req.continue(res => {
         res.body.data.data = []
       })
-    }).as("EmptyPreOrderTableResponse")
+    }).as("EmptyPreOrderTableResponse")*/
   })
 
   it('Nome do usuário logado é apresentado corretamente', () => {
